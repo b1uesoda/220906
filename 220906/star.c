d2c677f36f191ee35d0bbeb42068a05d66a68ef6
5584747 <b1uesoda@naver.com>
2022-09-06 10:30:25 +0900

    220906 실습

--- 220906/star.c
+++ 220906/star.c
@@ -6,9 +6,32 @@ int main() {
 
 	scanf_s("%d", &input);
 
-	for (int i = 0; i < input; i++) {
-		printf("*");
+	for (int i = 0; i < input; i++)
+	{
+		for (int a = input - i; a > 0; a--) {
+			printf("*");
+		}
+		for (int b = input - i; b < input; b++) {
+			printf("  ");
+		}
+		for (int a = input - i; a > 0; a--) {
+			printf("*");
+		}
+		printf("\n");
 	}
 
+	for (int i = 0; i < input; i++)
+	{
+		for (int b = input - i; b <= input; b++) {
+			printf("*");
+		}
+		for (int a = input - i - 1; a > 0; a--) {
+			printf("  ");
+		}
+		for (int b = input - i; b <= input; b++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
